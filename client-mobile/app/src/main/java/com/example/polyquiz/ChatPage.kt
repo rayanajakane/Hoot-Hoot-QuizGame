4bf86d9bc1ddfd2983136848da8ad875afb23bb0
Verocayden <112421679+Verocayden@users.noreply.github.com>
2025-01-16 11:42:51 -0500

    Started ChatPage

---
+++ client-mobile/app/src/main/java/com/example/polyquiz/ChatPage.kt
@@ -0,0 +1,9 @@
+package com.example.polyquiz
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ChatPage(modifier: Modifier) {
+
+}
--- client-mobile/app/src/main/java/com/example/polyquiz/MainActivity.kt
+++ client-mobile/app/src/main/java/com/example/polyquiz/MainActivity.kt
@@ -20,28 +20,17 @@ class MainActivity : ComponentActivity() {
         setContent {
             PolyQuizTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    ChatPage( modifier = Modifier.padding(innerPadding) )
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     PolyQuizTheme {
-        Greeting("Android")
+        ChatPage( modifier = Modifier.fillMaxSize() )
     }
 }
