3b23bb56bcba85f350571f5d2e78f5c9893cd816
Verocayden <112421679+Verocayden@users.noreply.github.com>
2025-01-16 14:23:08 -0500

    Added navigation and lifecycle dependencies

--- client-mobile/app/build.gradle.kts
+++ client-mobile/app/build.gradle.kts
@@ -41,6 +41,7 @@ android {
 
 dependencies {
     val coreVersion = "1.13.1"
+    val nav_version = "2.8.4"
     implementation("androidx.core:core-ktx:$coreVersion")
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -49,6 +50,8 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.7")
+    implementation("androidx.navigation:navigation-compose:$nav_version")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
