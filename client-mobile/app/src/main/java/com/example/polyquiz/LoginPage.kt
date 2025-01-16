d315a50608685eb99effba27971a0536794f0380
Verocayden <112421679+Verocayden@users.noreply.github.com>
2025-01-16 18:14:12 -0500

    Started upgrading

--- client-mobile/app/src/main/java/com/example/polyquiz/LoginPage.kt
+++ client-mobile/app/src/main/java/com/example/polyquiz/LoginPage.kt
@@ -15,6 +15,7 @@ import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.polyquiz.constants.DisplayAuthenticationText
+
 @Composable
 fun LoginPage() {
     Box(
--- client-mobile/app/src/main/java/com/example/polyquiz/SignupPage.kt
+++ client-mobile/app/src/main/java/com/example/polyquiz/SignupPage.kt
@@ -3,7 +3,6 @@ package com.example.polyquiz
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.ElevatedCard
@@ -17,6 +16,7 @@ import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.polyquiz.constants.DisplayAuthenticationText
+
 @Composable
 fun SignupPage() {
     Box(
