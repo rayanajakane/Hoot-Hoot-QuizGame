672fc25afcd57d59fa28367004857f0b715bd916
Verocayden <112421679+Verocayden@users.noreply.github.com>
2025-01-17 19:41:37 -0500

    Server Chat Gateway unit tests

--- server/app/gateways/chat/chat.gateway.spec.ts
+++ server/app/gateways/chat/chat.gateway.spec.ts
@@ -1,20 +1,27 @@
 /* eslint-disable max-lines */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { ChatService } from '@app/services/chat/chat.service';
+import { ChatEvents } from '@common/events/chat.events';
 import { Test, TestingModule } from '@nestjs/testing';
 import { SinonStubbedInstance, createStubInstance } from 'sinon';
-import { Server } from 'socket.io';
+import { Server, Socket } from 'socket.io';
 import { ChatGateway } from './chat.gateway';
+
+const mockMessage = {
+    text: '',
+    author: '',
+    date: new Date(),
+};
 
 describe('MatchGateway', () => {
     let gateway: ChatGateway;
     let chatSpy: SinonStubbedInstance<ChatService>;
-    // let socket: SinonStubbedInstance<Socket>;
+    let socket: SinonStubbedInstance<Socket>;
     let server: SinonStubbedInstance<Server>;
 
     beforeEach(async () => {
         chatSpy = createStubInstance(ChatService);
-        // socket = createStubInstance<Socket>(Socket);
+        socket = createStubInstance<Socket>(Socket);
         server = createStubInstance<Server>(Server);
 
         const module: TestingModule = await Test.createTestingModule({
@@ -40,27 +47,19 @@ describe('MatchGateway', () => {
         expect(gateway).toBeDefined();
     });
 
-    it('handleIncomingRoomMessages() should add the received message to the list of messages, and emit a newMessage event', () => {
-        /*
-        const mockMessageInfo = MOCK_MESSAGE_INFO;
-        const sendSpy = jest.spyOn(gateway, 'sendMessageToClients').mockReturnThis();
+    it('handlePrototypeMessage() should add the received message to the list of messages, and emit a newMessage event', () => {
+        const sendSpy = jest.spyOn(gateway, 'sendPrototypeMessageToClients').mockReturnThis();
         const addMessageSpy = jest.spyOn(chatSpy, 'addMessage').mockReturnThis();
-        gateway.handleIncomingRoomMessages(socket, mockMessageInfo);
-        expect(addMessageSpy).toHaveBeenCalledWith(mockMessageInfo.message, mockMessageInfo.roomCode);
-        expect(sendSpy).toHaveBeenCalledWith(MOCK_MESSAGE_INFO);
-        */
+        const validateMessageSpy = jest.spyOn(chatSpy, 'isValidMessage').mockReturnThis();
+        gateway.handlePrototypeMessage(socket, mockMessage);
+        expect(sendSpy).toHaveBeenCalled();
+        expect(addMessageSpy).toHaveBeenCalled();
+        expect(validateMessageSpy).toHaveBeenCalled();
     });
 
     it('sendMessageToClients() should emit a NewMessage event and send the messages to the players in the right room', () => {
-        /*
-        const toSpy = jest.spyOn(server, 'to').mockReturnValue({
-            emit: (event: string, messageInfo) => {
-                expect(event).toEqual('newMessage');
-                expect(messageInfo).toEqual(MOCK_MESSAGE_INFO);
-            },
-        } as unknown as BroadcastOperator<DefaultEventsMap, unknown>);
-        gateway.sendMessageToClients(MOCK_MESSAGE_INFO);
-        expect(toSpy).toHaveBeenCalledWith(MOCK_MESSAGE_INFO.roomCode);
-        */
+        const emitSpy = jest.spyOn(server, 'emit').mockReturnThis();
+        gateway.sendPrototypeMessageToClients(mockMessage);
+        expect(emitSpy).toHaveBeenCalledWith(ChatEvents.SentPrototypeMessage, mockMessage);
     });
 });
--- server/app/services/chat/chat.service.spec.ts
+++ server/app/services/chat/chat.service.spec.ts
@@ -1,6 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ChatService } from './chat.service';
 
+const validMessage = {
+    text: 'valid ',
+    author: '',
+    date: new Date(),
+};
+const secondValidMessage = {
+    text: 'valid 2',
+    author: '',
+    date: new Date(),
+};
+const emptyMessage = {
+    text: '',
+    author: '',
+    date: new Date(),
+};
+
 describe('ChatService', () => {
     let service: ChatService;
     beforeEach(async () => {
@@ -16,28 +32,13 @@ describe('ChatService', () => {
     });
 
     it('should return messages', () => {
-        const firstValidMessage = {
-            text: 'valid 1',
-            author: '',
-            date: new Date(),
-        };
-        const secondValidMessage = {
-            text: 'valid 2',
-            author: '',
-            date: new Date(),
-        };
-        service['messages'] = [firstValidMessage, secondValidMessage];
+        service['messages'] = [validMessage, secondValidMessage];
         expect(service.getMessages().length).toEqual(2);
-        expect(service.getMessages()[0]).toEqual(firstValidMessage);
+        expect(service.getMessages()[0]).toEqual(validMessage);
         expect(service.getMessages()[1]).toEqual(secondValidMessage);
     });
 
     it('should check if message is empty', () => {
-        const emptyMessage = {
-            text: '',
-            author: '',
-            date: new Date(),
-        };
         expect(service.isValidMessage(emptyMessage)).toBeFalsy();
     });
     it('should check if message is whitespace only', () => {
@@ -49,22 +50,12 @@ describe('ChatService', () => {
         expect(service.isValidMessage(whiteSpaceMessage)).toBeFalsy();
     });
     it('should check if message is valid', () => {
-        const validMessage = {
-            text: 'valid ',
-            author: '',
-            date: new Date(),
-        };
         expect(service.isValidMessage(validMessage)).toBeTruthy();
     });
 
     it('should add and return added message', () => {
         service['messages'] = [];
-        const addedMessage = {
-            text: 'valid ',
-            author: '',
-            date: new Date(),
-        };
-        expect(service.addMessage(addedMessage)).toEqual(addedMessage);
+        expect(service.addMessage(validMessage)).toEqual(validMessage);
         expect(service.getMessages().length).toEqual(1);
     });
 });
