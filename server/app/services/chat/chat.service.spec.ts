5366e674530e597410cd1aca1f476e470033166f
Verocayden <112421679+Verocayden@users.noreply.github.com>
2025-01-17 19:22:28 -0500

    Server ChatService unit tests

--- server/app/services/chat/chat.service.spec.ts
+++ server/app/services/chat/chat.service.spec.ts
@@ -15,24 +15,56 @@ describe('ChatService', () => {
         expect(service).toBeDefined();
     });
 
-    it('should add and get messages', () => {
-        /*
-        const matchRoomIndex = 0;
-        jest.spyOn(matchRoomService, 'getRoomIndex').mockReturnValue(matchRoomIndex);
-        service.addMessage(mockMessage, MOCK_MATCH_ROOM.code);
-        const messages = service.getMessages(mockRoomCode);
-        expect(messages).toEqual([mockMessage]);
-        const returnedMessage = service.addMessage(mockMessage, mockRoomCode);
-        expect(returnedMessage).toEqual(mockMessage);
-        */
+    it('should return messages', () => {
+        const firstValidMessage = {
+            text: 'valid 1',
+            author: '',
+            date: new Date(),
+        };
+        const secondValidMessage = {
+            text: 'valid 2',
+            author: '',
+            date: new Date(),
+        };
+        service['messages'] = [firstValidMessage, secondValidMessage];
+        expect(service.getMessages().length).toEqual(2);
+        expect(service.getMessages()[0]).toEqual(firstValidMessage);
+        expect(service.getMessages()[1]).toEqual(secondValidMessage);
     });
 
-    it('should return the added message', () => {
-        /*
-        const matchRoomIndex = 0;
-        jest.spyOn(matchRoomService, 'getRoomIndex').mockReturnValue(matchRoomIndex);
-        const returnedMessage = service.addMessage(mockMessage, matchRoomCode);
-        expect(returnedMessage).toEqual(mockMessage);
-        */
+    it('should check if message is empty', () => {
+        const emptyMessage = {
+            text: '',
+            author: '',
+            date: new Date(),
+        };
+        expect(service.isValidMessage(emptyMessage)).toBeFalsy();
+    });
+    it('should check if message is whitespace only', () => {
+        const whiteSpaceMessage = {
+            text: ' ',
+            author: '',
+            date: new Date(),
+        };
+        expect(service.isValidMessage(whiteSpaceMessage)).toBeFalsy();
+    });
+    it('should check if message is valid', () => {
+        const validMessage = {
+            text: 'valid ',
+            author: '',
+            date: new Date(),
+        };
+        expect(service.isValidMessage(validMessage)).toBeTruthy();
+    });
+
+    it('should add and return added message', () => {
+        service['messages'] = [];
+        const addedMessage = {
+            text: 'valid ',
+            author: '',
+            date: new Date(),
+        };
+        expect(service.addMessage(addedMessage)).toEqual(addedMessage);
+        expect(service.getMessages().length).toEqual(1);
     });
 });
