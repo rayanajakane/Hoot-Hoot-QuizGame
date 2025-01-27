4932c4189dc29a10713335645480f30d8803b80f
Rayan Ajakane <rayan.ajakane@polymtl.ca>
2025-01-27 19:03:21 +0000

    Edit environment.prod.ts for new deploy

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    serverUrl: 'http://ec2-99-79-48-140.ca-central-1.compute.amazonaws.com:3000/api',
-    serverUrlWithoutApi: 'http://ec2-99-79-48-140.ca-central-1.compute.amazonaws.com:3000/',
+    serverUrl: 'http://ec2-99-79-61-180.ca-central-1.compute.amazonaws.com:3000/api',
+    serverUrlWithoutApi: 'http://ec2-99-79-61-180.ca-central-1.compute.amazonaws.com:3000/',
 };
