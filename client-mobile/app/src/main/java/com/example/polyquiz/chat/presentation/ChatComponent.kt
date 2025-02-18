050dcec966758370f9d4e18f4f816da787711499
Sami-Ait-Ameur <113061454+Sami-Ait-Ameur@users.noreply.github.com>
2025-02-18 16:49:44 -0500

    scrolldown on new msg
    When a message is received, automatically scroll all the way down in the chat. Looks like it's working but having trouble testing the fix.

--- client-mobile/app/src/main/java/com/example/polyquiz/chat/presentation/ChatComponent.kt
+++ client-mobile/app/src/main/java/com/example/polyquiz/chat/presentation/ChatComponent.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
@@ -24,6 +25,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
@@ -51,12 +53,16 @@ fun ChatComponent(modifier: Modifier, authViewModel: AuthViewModel) {
     val messages by ChatService.messages.observeAsState()
     var newMessageText by remember{ mutableStateOf("") }
 
+    val listState = rememberLazyListState()
     Card(
         colors = CardDefaults.cardColors(
             containerColor = MaterialTheme.colorScheme.surfaceContainer
         ),
         shape = RoundedCornerShape(0.dp),
-        modifier = Modifier.size(width = 300.dp, height = 1000.dp).fillMaxHeight().imePadding()
+        modifier = Modifier
+            .size(width = 300.dp, height = 1000.dp)
+            .fillMaxHeight()
+            .imePadding()
     ) {
         Column(
             verticalArrangement = Arrangement.SpaceAround,
@@ -67,21 +73,34 @@ fun ChatComponent(modifier: Modifier, authViewModel: AuthViewModel) {
             // To handle the situation where there would be no message to display.
             messages?.let {
                 LazyColumn(
+                    state = listState,
                     verticalArrangement = Arrangement.spacedBy(10.dp),
-                    modifier = Modifier.weight(1f).padding(20.dp, 20.dp, 20.dp, 0.dp),
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(20.dp, 20.dp, 20.dp, 0.dp),
                 ) {
                     itemsIndexed(it) { _: Int, message: Message ->
                         MessageContainer(message, username)
                     }
                 }
+            LaunchedEffect(messages?.size) {
+                if (messages!!.isNotEmpty()) {
+                    listState.scrollToItem(messages!!.size - 1)
+                }
+            }
+
             } ?: LazyColumn(
                 verticalArrangement = Arrangement.spacedBy(10.dp),
-                modifier = Modifier.weight(1f).padding(20.dp, 20.dp, 20.dp, 0.dp)
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(20.dp, 20.dp, 20.dp, 0.dp)
             ) {
 
             }
             TextField(
-                modifier = Modifier.fillMaxWidth().padding(0.dp, 10.dp, 0.dp, 70.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(0.dp, 10.dp, 0.dp, 70.dp),
                 value = newMessageText,
                 onValueChange = { newText -> newMessageText = newText },
                 label = { Text(text = DisplayChatText.MESSAGE_LABEL.value) },
