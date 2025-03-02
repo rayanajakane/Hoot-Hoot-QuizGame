db0a179c403d48665d9dc690fb6f875edfacc35e
Verocayden <112421679+Verocayden@users.noreply.github.com>
2025-03-02 17:15:33 -0500

    Deactivated database to avoid bug

--- server/app/modules/firebase/firebase-repository/firebase-repository.service.ts
+++ server/app/modules/firebase/firebase-repository/firebase-repository.service.ts
@@ -1,7 +1,6 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { app } from 'firebase-admin';
 import { Auth } from 'firebase-admin/lib/auth/auth';
-import { Database } from 'firebase-admin/lib/database/database';
 
 // REFERENCE: https://medium.com/@elangoram1998/getting-started-with-firebase-admin-in-nest-js-71f676e73e6
 @Injectable()
@@ -9,12 +8,12 @@ export class FirebaseRepository {
     #db: FirebaseFirestore.Firestore;
     #collection: FirebaseFirestore.CollectionReference;
     auth: Auth;
-    database: Database;
+    // database: Database;
 
     constructor(@Inject('FIREBASE_APP') private firebaseApp: app.App) {
         this.#db = firebaseApp.firestore();
         this.#collection = this.#db.collection('<collection_name>');
         this.auth = firebaseApp.auth();
-        this.database = firebaseApp.database();
+        // this.database = firebaseApp.database();
     }
 }
