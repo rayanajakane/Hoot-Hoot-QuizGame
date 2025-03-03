90905f42eccc678d91995b3df7a2ffc29926a62e
Verocayden <112421679+Verocayden@users.noreply.github.com>
2025-03-02 19:18:25 -0500

    Removed some NgErrors

--- client/src/app/pages/signup-page/signup-page.component.spec.ts
+++ client/src/app/pages/signup-page/signup-page.component.spec.ts
@@ -1,4 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatCard } from '@angular/material/card';
+import { MatError, MatLabel } from '@angular/material/form-field';
+import { MatIcon } from '@angular/material/icon';
 import { SignupPageComponent } from '@app/pages/signup-page/signup-page.component';
 import { AuthenticationService } from '@app/services/authentication/authentication.service';
 import { getTranslocoModule } from '@app/transloco-testing.module';
@@ -12,7 +16,7 @@ describe('SignupPageComponent', () => {
     beforeEach(() => {
         const authenticationSpy = jasmine.createSpyObj('AuthenticationService', ['signUp']);
         TestBed.configureTestingModule({
-            imports: [getTranslocoModule()],
+            imports: [getTranslocoModule(), MatError, MatIcon, ReactiveFormsModule, MatLabel, MatCard],
             declarations: [SignupPageComponent],
             providers: [{ provide: AuthenticationService, useValue: authenticationSpy }],
         });
