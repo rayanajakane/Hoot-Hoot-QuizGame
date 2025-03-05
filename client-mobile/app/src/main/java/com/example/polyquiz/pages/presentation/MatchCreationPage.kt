c1bdce2ea686557801b3301eeaa9b0cf648748b9
Ikramarroud <ikram.arroud@polymtl.ca>
2025-03-05 11:48:22 -0500

    match-creation-page fixed

--- client-mobile/app/src/main/java/com/example/polyquiz/pages/presentation/MatchCreationPage.kt
+++ client-mobile/app/src/main/java/com/example/polyquiz/pages/presentation/MatchCreationPage.kt
@@ -1,9 +1,10 @@
-package com.example.polyquiz
+package com.example.polyquiz.pages.presentation
 
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -16,22 +17,37 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.unit.dp
+import com.example.polyquiz.Choice
+import com.example.polyquiz.Game
+import com.example.polyquiz.GameList
+import com.example.polyquiz.GameService
+import com.example.polyquiz.Question
+import com.example.polyquiz.SnackbarController
+import com.example.polyquiz.SnackbarEvent
 import com.example.polyquiz.auth.domain.AuthState
 import com.example.polyquiz.auth.domain.AuthViewModel
 import com.example.polyquiz.chat.presentation.ChatComponent
 import com.example.polyquiz.constants.AuthFeedbackText
 import com.example.polyquiz.constants.DisplayAuthenticationText
+import com.example.polyquiz.http.QuestionService
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.Locale
+
 
 @Composable
 fun MatchCreationPage(modifier: Modifier, navigateToLogin: () -> Unit, navigateToHome: () -> Unit, authViewModel: AuthViewModel) {
@@ -40,73 +56,38 @@ fun MatchCreationPage(modifier: Modifier, navigateToLogin: () -> Unit, navigateT
     val focusManager = LocalFocusManager.current
     val keyboardController = LocalSoftwareKeyboardController.current
     val questionService = QuestionService()
+    val gameService = GameService()
+    var games by remember { mutableStateOf<List<Game>>(emptyList()) }
+    var message by remember { mutableStateOf("") }
+    var isLoading by remember { mutableStateOf(false) }
 
-    fun getQuestionById(questionId: String) {
-        questionService.getQuestionById(questionId,
-            onSuccess = { question ->
-                println("Question: $question")
-                val result = questionService.convertToGenericType<Question>(question)
-                println("Result: $result")
-            },
-            onError = { errorMessage ->
-                println("Error: $errorMessage")
-            }
-        )
-    }
 
-    fun validateQuestion() {
-        val question = Question(id="", type="QCM", text="ça marche?", points=80.0, choices= listOf(Choice(text="yooo", isCorrect=true), Choice(text="ff", isCorrect=false), Choice(text="yosdfsoo", isCorrect=false), Choice(text="yoofsdfsdfo", isCorrect=false)), lastModification="")
-        questionService.verifyQuestion(question,
-            onSuccess = { response ->
-                println("Response: $response")
-            },
-            onError = { errorMessage ->
-                println("Error: $errorMessage")
-            }
-        )
-    }
+    fun fetchGames() {
+        gameService.getGames(
+            onSuccess = { games ->
+                val gson = Gson()
+                val json = gson.toJson(games)
+                val listType = object : TypeToken<List<Game>>() {}.type
+                val result: List<Game> = gson.fromJson(json, listType)
 
-    fun deleteQuestion(questionId: String) {
-        questionService.deleteQuestion(questionId,
-            onSuccess = { response ->
-                println("Response: $response")
-            },
-            onError = { errorMessage ->
-                println("Error: $errorMessage")
-            }
-        )
-    }
-
-    fun fetchQuestions() {
-        questionService.getAllQuestions(
-            onSuccess = { questions ->
-                println("Questions: $questions")
-                val result = questionService.convertToGenericType<List<Question>>(questions)
-                result?.forEach { question ->
-                    println("Question: ${question}")
+                result.forEach { game ->
+                    println("Game: ${game.title}")
                 }
+
             },
-            onError = { errorMessage ->
-                println("Error: $errorMessage")
-            }
+            onError = { errorMessage -> println("Error: $errorMessage") }
         )
     }
 
-    fun createQuestion() {
-        val question = Question(id="", type="QCM", text="ça marche?", points=80.0, choices= listOf(Choice(text="yooo", isCorrect=true), Choice(text="ff", isCorrect=false), Choice(text="yosdfsoo", isCorrect=false), Choice(text="yoofsdfsdfo", isCorrect=false)), lastModification="")
-        questionService.createQuestion(question,
-            onSuccess = { response ->
-                val result = questionService.convertToGenericType<Question>(response)
-                println("Response: $result")
-            },
-            onError = { errorMessage ->
-                println("Error: $errorMessage")
-            }
+    fun loadGames(service: GameService, onLoad: (List<Game>) -> Unit) {
+        service.getGames(
+            onSuccess = { onLoad(it) },
+            onError = { println("Erreur de chargement: $it") }
         )
     }
 
     LaunchedEffect(authState.value) {
-        when(authState.value) {
+        when (authState.value) {
             is AuthState.Unauthenticated -> {
                 scope.launch {
                     SnackbarController.sendEvent(
@@ -117,6 +98,7 @@ fun MatchCreationPage(modifier: Modifier, navigateToLogin: () -> Unit, navigateT
                 }
                 navigateToLogin()
             }
+
             is AuthState.Error -> {
                 scope.launch {
                     SnackbarController.sendEvent(
@@ -126,11 +108,11 @@ fun MatchCreationPage(modifier: Modifier, navigateToLogin: () -> Unit, navigateT
                     )
                 }
             }
+
             else -> Unit
         }
     }
-
-    Row (
+    Row(
         horizontalArrangement = Arrangement.SpaceBetween,
         modifier = Modifier
             .fillMaxSize()
@@ -140,91 +122,61 @@ fun MatchCreationPage(modifier: Modifier, navigateToLogin: () -> Unit, navigateT
                     keyboardController?.hide()
                 })
             }
-    ){
-        ChatComponent(modifier = modifier, authViewModel = authViewModel)
-        Column (
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center,
-            modifier = Modifier.fillMaxHeight()
+    ) {
+        ChatComponent(modifier = modifier.weight(1f), authViewModel = authViewModel)
+
+
+        Column(
+           horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Bottom,
+            modifier = Modifier
+                .weight(1f)
+                .padding(bottom = 10.dp)
         ) {
             Button(
                 onClick = {
-                    getQuestionById("b7a17fe8-dd3a-4751-a58d-b0b9d92a0173")
+                    fetchGames()
                 },
                 colors = ButtonDefaults.buttonColors(
                     containerColor = MaterialTheme.colorScheme.primary,
-                    contentColor = MaterialTheme.colorScheme.onPrimary)
-            ) {
-                Text(text = "Obtenir la question")
-            }
-            Button(
-                onClick = {
-                    validateQuestion()
-                },
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = MaterialTheme.colorScheme.primary,
-                    contentColor = MaterialTheme.colorScheme.onPrimary)
-            ) {
-                Text(text = "Valider question")
-            }
-            Button(
-                onClick = {
-                    deleteQuestion("1c0fed78-4a04-4d96-a0d6-b486aa31fb7d")
-                },
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = MaterialTheme.colorScheme.primary,
-                    contentColor = MaterialTheme.colorScheme.onPrimary)
-            ) {
-                Text(text = "Détruire question")
-            }
-            Button(
-                onClick = {
-                    fetchQuestions()
-                },
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = MaterialTheme.colorScheme.primary,
-                    contentColor = MaterialTheme.colorScheme.onPrimary)
+                    contentColor = MaterialTheme.colorScheme.onPrimary
+                ),
+                modifier = Modifier
+                    .padding(16.dp)
             ) {
                 Text(text = "Jouer")
             }
-            Button(
-                onClick = {
-                    createQuestion()
-                },
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = MaterialTheme.colorScheme.primary,
-                    contentColor = MaterialTheme.colorScheme.onPrimary)
-            ) {
-                Text(text = "Créer question")
-            }
+
             Surface(
                 shadowElevation = 10.dp,
                 tonalElevation = 10.dp,
                 color = MaterialTheme.colorScheme.surfaceColorAtElevation(10.dp),
                 modifier = Modifier.padding(10.dp)
-            ){
+            ) {
                 Button(
-                    onClick = {
-                        navigateToHome()
-                    },
+                    onClick = { navigateToHome() },
                     colors = ButtonDefaults.buttonColors(
                         containerColor = MaterialTheme.colorScheme.surfaceBright,
-                        contentColor = MaterialTheme.colorScheme.onSurface),
+                        contentColor = MaterialTheme.colorScheme.onSurface
+                    ),
                 ) {
                     Text(text = "Retourner à la page d'accueil")
                 }
             }
+
+
+            ElevatedButton(
+                onClick = { authViewModel.signOut() },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = MaterialTheme.colorScheme.surfaceBright,
+                    contentColor = MaterialTheme.colorScheme.onSurface
+                )
+            ) {
+                Text(text = DisplayAuthenticationText.LOGOUT.value)
+            }
+            GameList(modifier = modifier.weight(1f).fillMaxHeight(0.2f))
         }
-        ElevatedButton(
-            onClick = {
-                authViewModel.signOut()
-            },
-            modifier = Modifier.padding(20.dp),
-            colors = ButtonDefaults.buttonColors(
-                containerColor = MaterialTheme.colorScheme.surfaceBright,
-                contentColor = MaterialTheme.colorScheme.onSurface)
-        ) {
-            Text(text = DisplayAuthenticationText.LOGOUT.value)
-        }
+
     }
+
 }
