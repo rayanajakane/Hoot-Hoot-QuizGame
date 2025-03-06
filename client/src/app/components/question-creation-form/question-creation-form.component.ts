b72b4062f81e7962ef35bf5543ca570db8297443
Rayan Ajakane <rayan.ajakane@polymtl.ca>
2025-03-05 19:01:53 -0500

    Remove comment

--- client/src/app/components/question-creation-form/question-creation-form.component.ts
+++ client/src/app/components/question-creation-form/question-creation-form.component.ts
@@ -123,7 +123,6 @@ export class QuestionCreationFormComponent implements OnInit, OnChanges {
                 this.question.points = formValue?.points;
                 this.question.lastModification = new Date().toLocaleDateString();
                 this.question.choices = formValue?.choices;
-                // this.question.estimatedParameters = formValue?.estimatedParameters;
             });
         }
     }
