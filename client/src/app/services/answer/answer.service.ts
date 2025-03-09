620bdae7a1cf9975a0eaa7b9af8f91cf58b07cd9
Rayan Ajakane <rayan.ajakane@polymtl.ca>
2025-03-08 23:51:03 -0500

    Remove unused console logs

--- client/src/app/services/answer/answer.service.ts
+++ client/src/app/services/answer/answer.service.ts
@@ -109,7 +109,6 @@ export class AnswerService {
 
     onFeedback() {
         this.socketService.on(AnswerEvents.Feedback, (feedback: Feedback) => {
-            console.log(feedback);
             this.feedback = feedback;
             this.showFeedback = true;
             this.isNextQuestionButtonEnabled = true;
