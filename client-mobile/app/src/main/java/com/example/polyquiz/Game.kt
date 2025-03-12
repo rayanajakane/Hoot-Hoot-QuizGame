64ea8f934a8995c1b73f09f5f58827dada8817f9
Verocayden <112421679+Verocayden@users.noreply.github.com>
2025-03-11 20:28:29 -0400

    Added attributes to Game (mobile client side)

--- client-mobile/app/src/main/java/com/example/polyquiz/Game.kt
+++ client-mobile/app/src/main/java/com/example/polyquiz/Game.kt
@@ -4,12 +4,12 @@ import com.example.polyquiz.match.domain.Question
 
 data class Game(
     val id: String? ="",
-    // en cours ou en attente
-    //TODO: add mode
+    val originalId: String,
     val title: String,
     val description: String,
     val questions: List<Question>? = null,
     val lastModification: String?,
     val isVisible: Boolean?,
+    val nMatchesPlayed: Number,
     val duration: Number,
 )
