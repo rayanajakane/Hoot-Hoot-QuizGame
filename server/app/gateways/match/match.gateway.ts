9bf8ba34cfc8fb6a6e4fad8b85256764c74a768b
Verocayden <112421679+Verocayden@users.noreply.github.com>
2025-03-12 08:54:15 -0400

    Fixed typo + improved hostRoom currentQuestionIndex condition

--- server/app/gateways/match/match.gateway.ts
+++ server/app/gateways/match/match.gateway.ts
@@ -167,7 +167,7 @@ export class MatchGateway implements OnGatewayDisconnect {
         if (!hostRoomCode) return false;
         const hostRoom = this.matchRoomService.getRoom(hostRoomCode);
         socket.leave(hostRoomCode);
-        if (hostRoom.isPlaying || !hostRoom.currentQuestionIndex) {
+        if (hostRoom.isPlaying || hostRoom.currentQuestionIndex === 0) {
             this.sendError(hostRoomCode, NO_MORE_HOST);
             this.deleteRoom(hostRoomCode);
             return true;
