c5633b7518c7c2520f747529ff4459c86268c38b
nadabfh <112907983+nadabfh@users.noreply.github.com>
2025-03-19 09:59:03 -0400

    more fix for migration

--- client-mobile/app/src/main/java/com/example/polyquiz/match/presentation/QuestonArea.kt
+++ client-mobile/app/src/main/java/com/example/polyquiz/match/presentation/QuestonArea.kt
@@ -213,7 +213,7 @@ fun QuestionArea(
                         onClick = {
                             answerService.submitAnswer(
                                 UserInfo(
-                                    username = matchRoomService.retrieveUsername(),
+                                    username = matchRoomService.userId,
                                     roomCode = matchRoomService.getRoomCode()
                                 )
                             )
