31f1d14b00242605bf127f7f2f111cafd6c39151
Rayan Ajakane <rayan.ajakane@polymtl.ca>
2025-03-19 12:39:32 -0400

    Updated friend service to use sockets instead of http

--- client-mobile/app/src/main/java/com/example/polyquiz/constants/FriendsEvents.kt
+++ client-mobile/app/src/main/java/com/example/polyquiz/constants/FriendsEvents.kt
@@ -4,6 +4,11 @@ enum class FriendsEvents(val value: String) {
     REQUEST_SENT("requestSent"),
     REQUEST_ACCEPTED("requestAccepted"),
     REQUEST_REJECTED("requestRejected"),
-    REQUEST_CANCELLED("requestCanceled"),
-    FRIEND_REMOVED("friendRemoved")
+    REQUEST_CANCELED("requestCanceled"),
+    FRIEND_REMOVED("friendRemoved"),
+    RETURN_ALL_USERS("returnAllUsers"),
+    RETURN_ALL_FRIENDS("returnAllFriends"),
+    RETURN_ALL_PENDING_REQUESTS("returnAllPendingRequests"),
+    RETURN_ALL_SENT_REQUESTS("returnAllSentRequests"),
+    RETURN_ALL_DATA("returnAllData"),
 }
