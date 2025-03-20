e5777302f70417021ab84cd0fcec4f270e22ca47
Rayan Ajakane <rayan.ajakane@polymtl.ca>
2025-03-20 15:12:05 -0400

    remove unused console.log

--- server/app/gateways/match/match.gateway.ts
+++ server/app/gateways/match/match.gateway.ts
@@ -64,7 +64,6 @@ export class MatchGateway implements OnGatewayDisconnect {
         @ConnectedSocket() socket: Socket,
         @MessageBody() data: { gameId: string; hostId: string; isClassicMode: boolean; isFriendsOnly: boolean },
     ) {
-        console.log(data);
         if (data.isFriendsOnly) {
             const friendshipErrors = await this.friendService.getFriendshipErrors(data.hostId, true);
             if (friendshipErrors) {
