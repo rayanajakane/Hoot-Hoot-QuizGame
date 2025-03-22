d3568d4ee9f3726be71921e6f87c8371160d692f
Ikramarroud <ikram.arroud@polymtl.ca>
2025-03-22 14:41:36 -0400

    added response schema

--- server/app/services/questions-generator/questions-generator.service.ts
+++ server/app/services/questions-generator/questions-generator.service.ts
@@ -1,7 +1,8 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { OnEvent } from '@nestjs/event-emitter';
-import { ChatSession, GenerativeModel, GoogleGenerativeAI } from '@google/generative-ai';
+import { ChatSession, GenerativeModel, GoogleGenerativeAI, Schema, SchemaType } from '@google/generative-ai';
+
 import { GenerateQuestionDto } from '@app/model/dto/question/generate-question.dto';
 import { v4 } from 'uuid';
 
@@ -9,35 +10,56 @@ const GEMINI_MODEL = "gemini-1.5-flash";
 
 //Sources/tutorials: 
 //https://www.youtube.com/watch?v=gBLrCdkZTec&ab_channel=ComputingPower
+//https://cloud.google.com/vertex-ai/generative-ai/docs/multimodal/control-generated-output
 
 @Injectable()
 export class QuestionsGeneratorService {
     private readonly googleAI: GoogleGenerativeAI;
     private readonly model: GenerativeModel;
-    private chatSessions: {[sessionId: string]: ChatSession} = {}; //ChatSession comes from Gemini
+    private chatSessions: {[sessionId: string]: ChatSession} = {}; 
 
     private readonly logger = new Logger(QuestionsGeneratorService.name); 
-
-    
+    schema: Schema = {
+        type: SchemaType.ARRAY,
+        items: {
+          type: SchemaType.OBJECT,
+          properties: {
+            Question: {
+              type: SchemaType.STRING,
+              nullable: false,
+            },
+            Response: {
+                type: SchemaType.STRING,
+                nullable: false,
+              },
+          },
+        },
+      };
     constructor (configService: ConfigService){
         const geminiApiKey = "AIzaSyDtfJfe29-BN22yt8RDUboSdFb7LXWKHyo";
-        this.googleAI = new GoogleGenerativeAI(geminiApiKey);
-        this.model = this.googleAI.getGenerativeModel({ model: GEMINI_MODEL });
+        this.googleAI = new GoogleGenerativeAI(geminiApiKey);    
+        this.model = this.googleAI.getGenerativeModel({ model: GEMINI_MODEL ,generationConfig :{
+            responseMimeType: "application/json",
+            responseSchema : this.schema,
+        } });
     }
 
     async generateText(data: GenerateQuestionDto){
         try{
             const{sessionId,chat} = this.getChatSession(data.sessionId);
+            console.log(data.prompt)    
             const result = await chat.sendMessage(data.prompt);
+
             return{
                 return: await result.response.text(),
+                
                 sessionId
+                
         }
         }
         catch(error){
             this.logger.error("Gemini error", error);
         }
-    
     }
 
     private getChatSession(sessionId?:string){
