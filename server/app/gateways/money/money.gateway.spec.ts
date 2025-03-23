98ac22c92a92b0c9002e5205b2a8df86dc13de2a
nadabfh <112907983+nadabfh@users.noreply.github.com>
2025-03-23 01:21:11 -0400

    fixed tests server side

--- server/app/gateways/money/money.gateway.spec.ts
+++ server/app/gateways/money/money.gateway.spec.ts
@@ -1,18 +1,35 @@
+import { FirebaseAuthService } from '@app/modules/firebase/firebase-auth/firebase-auth.service';
+import { MoneyService } from '@app/services/money/money.service';
 import { Test, TestingModule } from '@nestjs/testing';
 import { MoneyGateway } from './money.gateway';
 
 describe('MoneyGateway', () => {
-  let gateway: MoneyGateway;
+    let gateway: MoneyGateway;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [MoneyGateway],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [
+                MoneyGateway,
+                {
+                    provide: MoneyService,
+                    useValue: {
+                        getCurrentBalance: jest.fn().mockResolvedValue(0),
+                        updateBalance: jest.fn().mockResolvedValue(0),
+                        donateMoney: jest.fn().mockResolvedValue(true),
+                        getMoneyError: jest.fn().mockResolvedValue(null),
+                    },
+                },
+                {
+                    provide: FirebaseAuthService,
+                    useValue: { getUserById: jest.fn().mockResolvedValue({ displayName: 'TestUser' }) },
+                },
+            ],
+        }).compile();
 
-    gateway = module.get<MoneyGateway>(MoneyGateway);
-  });
+        gateway = module.get<MoneyGateway>(MoneyGateway);
+    });
 
-  it('should be defined', () => {
-    expect(gateway).toBeDefined();
-  });
+    it('should be defined', () => {
+        expect(gateway).toBeDefined();
+    });
 });
