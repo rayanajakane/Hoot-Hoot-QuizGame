8169726b4d8ff16536ef6b7a5c15ee2b35d20487
nadabfh <112907983+nadabfh@users.noreply.github.com>
2025-03-24 14:26:28 -0400

    fixed test client side

--- client/src/app/components/party-config-dialog/party-config-dialog.component.spec.ts
+++ client/src/app/components/party-config-dialog/party-config-dialog.component.spec.ts
@@ -1,23 +1,29 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 import { PartyConfigDialogComponent } from './party-config-dialog.component';
 
 describe('PartyConfigDialogComponent', () => {
-  let component: PartyConfigDialogComponent;
-  let fixture: ComponentFixture<PartyConfigDialogComponent>;
+    let component: PartyConfigDialogComponent;
+    let fixture: ComponentFixture<PartyConfigDialogComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [PartyConfigDialogComponent]
-    })
-    .compileComponents();
-    
-    fixture = TestBed.createComponent(PartyConfigDialogComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [PartyConfigDialogComponent],
+            providers: [
+                { provide: MatDialogRef, useValue: {} },
+                { provide: MAT_DIALOG_DATA, useValue: {} },
+            ],
+        }).compileComponents();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(PartyConfigDialogComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
