abf5d4c41279a5fbdbd02fc5a9ecd1c3c2bae4bf
Rayan Ajakane <rayan.ajakane@polymtl.ca>
2025-03-24 19:11:26 -0400

    buy avatar completed on cliented side, server side missing

--- client/src/app/pages/shop-page/shop-page.component.ts
+++ client/src/app/pages/shop-page/shop-page.component.ts
@@ -1,15 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { PremiumAvatar } from '@app/constants/image-constants';
+import { AuthenticationService } from '@app/services/authentication/authentication.service';
 import { AvatarService } from '@app/services/avatar/avatar.service';
 import { MoneyService } from '@app/services/money/money.service';
-import { NotificationService } from '@app/services/notification/notification.service';
-
-interface ShopItem {
-    id: string;
-    imageUrl: string;
-    price: number;
-    owned: boolean;
-}
+import { SocketHandlerService } from '@app/services/socket-handler/socket-handler.service';
+import { MoneyEvents } from '@common/events/money.events';
+import { ShopItem } from '@common/interfaces/shop-item';
 
 @Component({
     selector: 'app-shop-page',
@@ -23,26 +19,45 @@ export class ShopPageComponent implements OnInit {
     constructor(
         public moneyService: MoneyService,
         private avatarService: AvatarService,
-        private notificationService: NotificationService,
+        // private notificationService: NotificationService,
+        private readonly authService: AuthenticationService,
+        private readonly socketHandler: SocketHandlerService,
     ) {}
 
     async ngOnInit() {
         await this.initializeShopItems();
     }
 
-    async purchaseAvatar(item: ShopItem) {
-        if (this.moneyService.currentBalance >= item.price) {
-            try {
-                await this.avatarService.purchaseAvatar(item.id);
-                // await this.moneyService.decreaseBalance(item.price);
-                item.owned = true;
-                this.notificationService.displaySuccessMessage('Avatar purchased successfully!');
-            } catch (error) {
-                this.notificationService.displayErrorMessage('Failed to purchase avatar');
+    // async purchaseAvatar(item: ShopItem) {
+    //     if (this.moneyService.currentBalance >= item.price) {
+    //         try {
+    //             await this.avatarService.purchaseAvatar(item.id);
+    //             // await this.moneyService.decreaseBalance(item.price);
+    //             item.owned = true;
+    //             this.notificationService.displaySuccessMessage('Avatar purchased successfully!');
+    //         } catch (error) {
+    //             this.notificationService.displayErrorMessage('Failed to purchase avatar');
+    //         }
+    //     } else {
+    //         this.notificationService.displayErrorMessage('Insufficient funds!');
+    //     }
+    // }
+
+    buyAvatar(item: ShopItem) {
+        this.socketHandler.send(MoneyEvents.BuyAvatar, {
+            user: this.authService.userId,
+            item,
+        });
+    }
+
+    onAvatarBought() {
+        this.socketHandler.on(MoneyEvents.AvatarBought, (item: ShopItem) => {
+            this.avatarService.purchaseAvatar(item.id);
+            const foundAvatar = this.avatarItems.find((avatar) => avatar.id === item.id);
+            if (foundAvatar) {
+                foundAvatar.owned = true;
             }
-        } else {
-            this.notificationService.displayErrorMessage('Insufficient funds!');
-        }
+        });
     }
 
     private async initializeShopItems() {
---
+++ common/interfaces/shop-item.ts
@@ -0,0 +1,6 @@
+export interface ShopItem {
+    id: string;
+    imageUrl: string;
+    price: number;
+    owned: boolean;
+}
