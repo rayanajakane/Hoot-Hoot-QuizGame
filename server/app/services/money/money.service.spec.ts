cff6d5c32f49c75706931b4f6bf7c6c939fcae11
Rayan Ajakane <rayan.ajakane@polymtl.ca>
2025-03-24 14:14:07 -0400

    Fixed all tests on server side

--- server/app/services/money/money.service.spec.ts
+++ server/app/services/money/money.service.spec.ts
@@ -1,12 +1,18 @@
 import { FirebaseAuthService } from '@app/modules/firebase/firebase-auth/firebase-auth.service';
 import { FirebaseRepositoryService } from '@app/modules/firebase/firebase-repository/firebase-repository.service';
+import { MatchRoomService } from '@app/services/match-room/match-room.service';
 import { Test, TestingModule } from '@nestjs/testing';
+import { SinonStubbedInstance, createStubInstance } from 'sinon';
 import { MoneyService } from './money.service';
 
 describe('MoneyService', () => {
     let service: MoneyService;
+    let firebaseAuthSpy: SinonStubbedInstance<FirebaseAuthService>;
+    let matchRoomSpy: SinonStubbedInstance<MatchRoomService>;
 
     beforeEach(async () => {
+        firebaseAuthSpy = createStubInstance(FirebaseAuthService);
+        matchRoomSpy = createStubInstance(MatchRoomService);
         const module: TestingModule = await Test.createTestingModule({
             providers: [
                 MoneyService,
@@ -22,7 +28,8 @@ describe('MoneyService', () => {
                         },
                     },
                 },
-                { provide: FirebaseAuthService, useValue: {} },
+                { provide: FirebaseAuthService, useValue: firebaseAuthSpy },
+                { provide: MatchRoomService, useValue: matchRoomSpy },
             ],
         }).compile();
 
