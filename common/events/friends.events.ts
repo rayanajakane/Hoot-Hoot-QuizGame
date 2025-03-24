1731bfa386b1ad5c9ed656615bd7b078d8db8d31
Verocayden <112421679+Verocayden@users.noreply.github.com>
2025-03-23 21:31:14 -0400

    Fixed wrong event name

--- common/events/friends.events.ts
+++ common/events/friends.events.ts
@@ -10,5 +10,5 @@ export enum FriendsEvents {
     ReturnAllSentRequests = 'returnAllSentRequests',
     ReturnAllData = 'returnAllData',
     UpdateData = 'updateData',
-    Connect = 'connect',
+    Connect = 'connectUser',
 }
