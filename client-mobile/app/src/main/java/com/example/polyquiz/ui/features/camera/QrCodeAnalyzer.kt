49164009c7ef5d8cd89ede34ee629c3cd782e6b2
Hiba Chaarani <hiba.chaarani@polymtl.ca>
2025-03-24 13:35:13 -0400

    Redid QrCodeAnalyzer

--- client-mobile/app/src/main/java/com/example/polyquiz/ui/features/camera/QrCodeAnalyzer.kt
+++ client-mobile/app/src/main/java/com/example/polyquiz/ui/features/camera/QrCodeAnalyzer.kt
@@ -1,77 +1,80 @@
 package com.example.polyquiz.ui.features.camera
 
-import android.graphics.ImageFormat.YUV_420_888
-import android.graphics.ImageFormat.YUV_422_888
-import android.graphics.ImageFormat.YUV_444_888
-import android.util.Log
+import android.graphics.ImageFormat
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageProxy
 import com.google.zxing.BarcodeFormat
 import com.google.zxing.BinaryBitmap
 import com.google.zxing.DecodeHintType
 import com.google.zxing.MultiFormatReader
-import com.google.zxing.NotFoundException
 import com.google.zxing.PlanarYUVLuminanceSource
 import com.google.zxing.common.HybridBinarizer
 import java.nio.ByteBuffer
 
-private fun ByteBuffer.toByteArray(): ByteArray {
-    rewind()
-    val data = ByteArray(remaining())
-    get(data)
-    return data
-}
+// Ref : Our lord and savior Philipp Lackner
+// https://www.youtube.com/watch?v=asl1mFtkMkc
 
 class QrCodeAnalyzer(
-    private val onQrCodesDetected: (qrCode: Result<com.google.zxing.Result>) -> Unit
-) : ImageAnalysis.Analyzer {
+    private val onQrCodeScanned: (String) -> Unit
+): ImageAnalysis.Analyzer {
 
-    private val yuvFormats = mutableListOf(YUV_420_888)
-
-    init {
-
-        yuvFormats.addAll(listOf(YUV_422_888, YUV_444_888))
-
-    }
-
-    private val reader = MultiFormatReader().apply {
-        val map = mapOf(
-            DecodeHintType.POSSIBLE_FORMATS to arrayListOf(BarcodeFormat.QR_CODE)
-        )
-        setHints(map)
-    }
+    // To be able to scan QR codes
+    private val supportedImageFormats = listOf(
+        ImageFormat.YUV_420_888,
+        ImageFormat.YUV_422_888,
+        ImageFormat.YUV_444_888
+    )
 
     override fun analyze(image: ImageProxy) {
-        // We are using YUV format because, ImageProxy internally uses ImageReader to get the image
-        // by default ImageReader uses YUV format unless changed.
-        if (image.format !in yuvFormats) {
-            Log.e("QRCodeAnalyzer", "Expected YUV, now = ${image.format}")
-            return
+        if(image.format in supportedImageFormats) {
+            // Get raw data
+            val bytes = image.planes.first().buffer.toByteArray()
+
+            val source = PlanarYUVLuminanceSource(
+                bytes,
+                image.width,
+                image.height,
+                0,
+                0,
+                image.width,
+                image.height,
+                false
+            )
+
+            // Bitmap file with info about QR code
+            val binaryBmp = BinaryBitmap(HybridBinarizer(source))
+
+            // Need to decode
+            // Need to put in try-catch block because will throw exception everytime no QRcode in frame
+            // TODO : Check device rotation. Make sure QR is recognised
+            try{
+                val result = MultiFormatReader().apply {
+                    // Defined map of supported types of codes to scan
+                    setHints(
+                        mapOf(
+                            DecodeHintType.POSSIBLE_FORMATS to arrayListOf(
+                                BarcodeFormat.QR_CODE
+                            )
+                        )
+                    )
+                }.decode(binaryBmp)
+
+                onQrCodeScanned(result.text)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            } finally {
+                // Free resources
+                image.close()
+            }
+
         }
+    }
 
-        val data = image.planes[0].buffer.toByteArray()
-
-        val source = PlanarYUVLuminanceSource(
-            data,
-            image.width,
-            image.height,
-            0,
-            0,
-            image.width,
-            image.height,
-            false
-        )
-
-        val binaryBitmap = BinaryBitmap(HybridBinarizer(source))
-        try {
-            // Whenever reader fails to detect a QR code in image
-            // it throws NotFoundException
-            val result = reader.decode(binaryBitmap)
-            onQrCodesDetected(Result.success(result))
-        } catch (e: NotFoundException) {
-            e.printStackTrace()
-            onQrCodesDetected(Result.failure(e))
-        }
-        image.close()
+    // Function to convert buffer to byte array
+    private fun ByteBuffer.toByteArray(): ByteArray {
+        rewind() // To start at the beginning of our byteArray
+        return ByteArray(remaining()).also {
+            get(it) // Get all bytes in the byteArray
+        } // Include the whole byte buffer
     }
 }
