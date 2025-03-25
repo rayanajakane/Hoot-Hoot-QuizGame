0b13498faaf1bf7da9cd2eec5c88d30994b9c4f0
Verocayden <112421679+Verocayden@users.noreply.github.com>
2025-03-25 10:13:37 -0400

    Fixed desktop client tests imports

--- client/src/app/services/history/history.service.spec.ts
+++ client/src/app/services/history/history.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
+import { HttpClient, HttpHandler } from '@angular/common/http';
 import { HistoryService } from './history.service';
 
 describe('HistoryService', () => {
     let service: HistoryService;
 
     beforeEach(() => {
-        TestBed.configureTestingModule({});
+        TestBed.configureTestingModule({
+            providers: [HttpClient, HttpHandler],
+        });
         service = TestBed.inject(HistoryService);
     });
 
