a09895f27962d0efc6246f76bd808886422adaa1
Rayan Ajakane <rayan.ajakane@polymtl.ca>
2025-03-26 12:22:28 -0400

    added missing providers to test

--- client/src/app/components/question-generator/question-generator.component.spec.ts
+++ client/src/app/components/question-generator/question-generator.component.spec.ts
@@ -1,23 +1,31 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { ManagementState } from '@app/constants/states';
+import { DialogManagement, QuestionService } from '@app/services/question/question.service';
 import { QuestionGeneratorComponent } from './question-generator.component';
 
 describe('QuestionGeneratorComponent', () => {
-  let component: QuestionGeneratorComponent;
-  let fixture: ComponentFixture<QuestionGeneratorComponent>;
+    let component: QuestionGeneratorComponent;
+    let fixture: ComponentFixture<QuestionGeneratorComponent>;
+    const dialogData: DialogManagement = { modificationState: ManagementState.GameCreate };
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [QuestionGeneratorComponent]
-    })
-    .compileComponents();
-    
-    fixture = TestBed.createComponent(QuestionGeneratorComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [QuestionGeneratorComponent],
+            providers: [
+                { provide: MAT_DIALOG_DATA, useValue: dialogData },
+                { provide: MatDialogRef, useValue: jasmine.createSpyObj('MatDialogRef', ['close']) },
+                { provide: QuestionService, useValue: jasmine.createSpyObj('QuestionService', ['validateChoicesLength']) },
+            ],
+        }).compileComponents();
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+        fixture = TestBed.createComponent(QuestionGeneratorComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
