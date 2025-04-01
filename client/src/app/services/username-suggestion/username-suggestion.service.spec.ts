d242b56b12a5582efab95bb46c7a96fc5ed3deaa
Verocayden <112421679+Verocayden@users.noreply.github.com>
2025-04-01 17:21:00 -0400

    Fixed client unit tests

--- client/src/app/services/username-suggestion/username-suggestion.service.spec.ts
+++ client/src/app/services/username-suggestion/username-suggestion.service.spec.ts
@@ -1,12 +1,17 @@
+import { HttpClient, HttpHandler } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 
 import { UsernameSuggestionService } from '@app/services/username-suggestion/username-suggestion.service';
+import { getTranslocoModule } from '@app/transloco-testing.module';
 
 describe('UsernameSuggestionService', () => {
     let service: UsernameSuggestionService;
 
     beforeEach(() => {
-        TestBed.configureTestingModule({});
+        TestBed.configureTestingModule({
+            imports: [getTranslocoModule()],
+            providers: [HttpClient, HttpHandler],
+        });
         service = TestBed.inject(UsernameSuggestionService);
     });
 
