2287111ea172d58095d95971cc802715ca01dee7
Sami-Ait-Ameur <113061454+Sami-Ait-Ameur@users.noreply.github.com>
2025-04-01 14:35:30 -0400

    elo service shows current elo

--- client-mobile/app/src/main/java/com/example/polyquiz/elo/presentation/RankingsPage.kt
+++ client-mobile/app/src/main/java/com/example/polyquiz/elo/presentation/RankingsPage.kt
@@ -9,9 +9,16 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -23,9 +30,11 @@ import androidx.compose.ui.unit.sp
 import com.example.polyquiz.R
 import com.example.polyquiz.auth.domain.AuthViewModel
 import com.example.polyquiz.chat.presentation.ChatComponent
+import com.example.polyquiz.constants.FriendsDisplayText
 import com.example.polyquiz.elo.domain.EloService
 import com.example.polyquiz.ui.MenuButton
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun RankingsPage(
     modifier: Modifier,
@@ -41,10 +50,6 @@ fun RankingsPage(
     val rankings by EloService._rankings.observeAsState(emptyList())
     val currentRating by EloService.currentRating.observeAsState(0)
 
-//    fun quitRankingsPage() {
-//        EloService.stopListeningForEloEvents()
-//        navigateToHome()
-//    }
     LaunchedEffect(Unit) {
         EloService.listenForEloEvents()
         EloService.getElo(authViewModel.getUserId())
