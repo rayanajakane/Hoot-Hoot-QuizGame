44dfd5bde0941efd021b14ecfe02add97b3e05f1
Rayan Ajakane <rayan.ajakane@polymtl.ca>
2025-04-01 14:29:11 -0400

    fixed tests

--- client/src/app/services/avatar/avatar.service.spec.ts
+++ client/src/app/services/avatar/avatar.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
-
+import { authStub } from '@app/constants/auth-mocks';
+import { AuthenticationService } from '@app/services/authentication/authentication.service';
 import { AvatarService } from '@app/services/avatar/avatar.service';
 
 describe('AvatarService', () => {
     let service: AvatarService;
 
     beforeEach(() => {
-        TestBed.configureTestingModule({});
+        TestBed.configureTestingModule({
+            providers: [AvatarService, { provide: AuthenticationService, useValue: authStub }],
+        });
         service = TestBed.inject(AvatarService);
     });
 
