f2f20d1f2130d46ca79cedaa4613d3bc3969ec62
Sami-Ait-Ameur <113061454+Sami-Ait-Ameur@users.noreply.github.com>
2025-04-01 14:46:14 -0400

    elo fully functionnal everywhere (server and both clients)

--- client-mobile/app/src/main/java/com/example/polyquiz/elo/presentation/RankingsPage.kt
+++ client-mobile/app/src/main/java/com/example/polyquiz/elo/presentation/RankingsPage.kt
@@ -74,7 +74,7 @@ fun RankingsPage(
                 Row(modifier = Modifier.fillMaxWidth().padding(vertical = 4.dp), horizontalArrangement = Arrangement.SpaceBetween) {
                     Text(text = "${index + 1}", modifier = Modifier.weight(1f))
                     Text(text = ranking.username, modifier = Modifier.weight(2f))
-                    Text(text = ranking.rating.toString(), modifier = Modifier.weight(1f))
+                    Text(text = ranking.rating.roundToInt().toString(), modifier = Modifier.weight(1f))
                 }
             }
         }
