fe78f462d272ff198b737393e776deaee2bf2b73
Verocayden <112421679+Verocayden@users.noreply.github.com>
2025-04-01 17:28:26 -0400

    Fixed server tests

--- server/app/controllers/username-suggestion/username-suggestion.controller.spec.ts
+++ server/app/controllers/username-suggestion/username-suggestion.controller.spec.ts
@@ -1,12 +1,22 @@
+import { UsernameSuggestionService } from '@app/services/username-suggestion/username-suggestion.service';
 import { Test, TestingModule } from '@nestjs/testing';
+import { createStubInstance, SinonStubbedInstance } from 'sinon';
 import { UsernameSuggestionController } from './username-suggestion.controller';
 
 describe('UsernameSuggestionController', () => {
     let controller: UsernameSuggestionController;
+    let suggestionService: SinonStubbedInstance<UsernameSuggestionService>;
 
     beforeEach(async () => {
+        suggestionService = createStubInstance(UsernameSuggestionService);
         const module: TestingModule = await Test.createTestingModule({
             controllers: [UsernameSuggestionController],
+            providers: [
+                {
+                    provide: UsernameSuggestionService,
+                    useValue: suggestionService,
+                },
+            ],
         }).compile();
 
         controller = module.get<UsernameSuggestionController>(UsernameSuggestionController);
--- server/app/services/username-suggestion/username-suggestion.service.spec.ts
+++ server/app/services/username-suggestion/username-suggestion.service.spec.ts
@@ -1,12 +1,22 @@
+import { FirebaseAuthService } from '@app/modules/firebase/firebase-auth/firebase-auth.service';
 import { Test, TestingModule } from '@nestjs/testing';
+import { createStubInstance, SinonStubbedInstance } from 'sinon';
 import { UsernameSuggestionService } from './username-suggestion.service';
 
 describe('UsernameSuggestionService', () => {
     let service: UsernameSuggestionService;
+    let authService: SinonStubbedInstance<FirebaseAuthService>;
 
     beforeEach(async () => {
+        authService = createStubInstance(FirebaseAuthService);
         const module: TestingModule = await Test.createTestingModule({
-            providers: [UsernameSuggestionService],
+            providers: [
+                UsernameSuggestionService,
+                {
+                    provide: FirebaseAuthService,
+                    useValue: authService,
+                },
+            ],
         }).compile();
 
         service = module.get<UsernameSuggestionService>(UsernameSuggestionService);
