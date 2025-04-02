5f0c4674f340e26423b15746760678bbcaabd4f9
nadabfh <112907983+nadabfh@users.noreply.github.com>
2025-04-02 17:55:49 -0400

    all translation texts done

--- server/app/constants/match-errors.ts
+++ server/app/constants/match-errors.ts
@@ -1,6 +1,6 @@
 // TODO : remove
 
-export const BAN_PLAYER = '.ban_player';
-export const NO_MORE_HOST = '.no_more_host';
-export const NO_MORE_PLAYERS = '.no_more_players';
+export const BAN_PLAYER = 'match-errors.ban_player';
+export const NO_MORE_HOST = 'match-errors.no_more_host';
+export const NO_MORE_PLAYERS = 'match-errors.no_more_players';
 export const LESS_THAN_3_PLAYERS = 'Il faut au moins 3 joueurs pour jouer.';
--- server/app/constants/match-login-errors.ts
+++ server/app/constants/match-login-errors.ts
@@ -8,7 +8,7 @@ const INVALID_CODE = 'Le code est invalide. ';
 const LOCKED_ROOM = 'La salle est verrouillée. ';
 const EMPTY_USERNAME = 'Le nom ne doit pas être vide. ';
 
-const NOT_FRIENDS_WITH_HOST = "Vous n'êtes pas ami avec l'hôte. ";
-const NO_FRIENDS = "Vous n'avez pas d'amis. ";
+const NOT_FRIENDS_WITH_HOST = 'match-friends-errors.not_friends_with_host';
+const NO_FRIENDS = 'match-friends-errors.no_friends';
 
 export { BANNED_PLAYER, EMPTY_USERNAME, HOST_CONFLICT, INVALID_CODE, LOCKED_ROOM, NO_FRIENDS, NOT_FRIENDS_WITH_HOST, USED_USERNAME };
