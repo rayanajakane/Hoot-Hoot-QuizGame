f2bdd16266270d81773482c607054ed059215f8f
nadabfh <112907983+nadabfh@users.noreply.github.com>
2025-04-02 17:55:49 -0400

    all translation texts done

--- server/app/constants/game-validation-errors.ts
+++ server/app/constants/game-validation-errors.ts
@@ -1,31 +1,20 @@
-import {
-    MAX_CHOICES_NUMBER,
-    MAX_DURATION,
-    MAX_MARGIN,
-    MAX_POINTS,
-    MIN_CHOICES_NUMBER,
-    MIN_DURATION,
-    MIN_POINTS,
-    STEP_POINTS,
-} from './game-validation-constraints';
-
-const ERROR_REPEAT_CHOICES = 'Il ne doit pas y avoir deux choix identiques dans une question.';
-const ERROR_CHOICES_NUMBER = `Il doit y avoir entre ${MIN_CHOICES_NUMBER} et ${MAX_CHOICES_NUMBER} choix.`;
-const ERROR_POINTS = `Les points doivent être entre ${MIN_POINTS} et ${MAX_POINTS} et divisibles par ${STEP_POINTS}`;
-const ERROR_EMPTY_QUESTION = 'Le texte de la question ne doit pas être vide.';
-const ERROR_CHOICES_RATIO = 'Il doit y avoir au moins un choix valide et un choix invalide.';
-const ERROR_EMPTY_TITLE = 'Le titre du jeu est vide.';
-const ERROR_EMPTY_DESCRIPTION = 'La description du jeu est vide.';
-const ERROR_DURATION = `La durée doit être entre ${MIN_DURATION} et ${MAX_DURATION} secondes.`;
-const ERROR_QUESTIONS_NUMBER = 'Le jeu doit avoir au moins une question.';
-const ERROR_QUESTION_TYPE = 'Le type de la question doit être QCM ou QRL';
-const ERROR_INVALID_BOUNDS = 'La borne inférieure doit être inférieure à la borne supérieure.';
-const ERROR_MARGIN_TOO_BIG = `La marge doit est positive et ne doit representer que le ${MAX_MARGIN}% de l'intervalle`;
-const ERROR_ANSWER_OUT_OF_BOUNDS = 'La réponse doit être entre les deux bornes.';
-const ERROR_ANSWER_NOT_INTEGER = 'La réponse doit être un entier.';
-const ERROR_LOWER_BOUND_NOT_INTEGER = 'La borne inférieure doit être un entier.';
-const ERROR_UPPER_BOUND_NOT_INTEGER = 'La borne supérieure doit être un entier.';
-const ERROR_MARGIN_NOT_INTEGER = 'La marge doit être un entier.';
+const ERROR_REPEAT_CHOICES = 'game-validation-errors.error-repeat-choices';
+const ERROR_CHOICES_NUMBER = `game-validation-errors.error-choices-number`;
+const ERROR_POINTS = `game-validation-errors.error-points`;
+const ERROR_EMPTY_QUESTION = 'game-validation-errors.error-empty-question';
+const ERROR_CHOICES_RATIO = 'game-validation-errors.error-choices-ratio';
+const ERROR_EMPTY_TITLE = 'game-validation-errors.error-empty-title';
+const ERROR_EMPTY_DESCRIPTION = 'game-validation-errors.error-empty-description';
+const ERROR_DURATION = `game-validation-errors.error-duration`;
+const ERROR_QUESTIONS_NUMBER = 'game-validation-errors.error-question-number';
+const ERROR_QUESTION_TYPE = 'game-validation-errors.error-question-type';
+const ERROR_INVALID_BOUNDS = 'game-validation-errors.error-invalid-bounds';
+const ERROR_MARGIN_TOO_BIG = `game-validation-errors.error-margin-too-big`;
+const ERROR_ANSWER_OUT_OF_BOUNDS = 'game-validation-errors.error-answer-out-of-bounds.';
+const ERROR_ANSWER_NOT_INTEGER = 'game-validation-errors.error-answer-not-integer';
+const ERROR_LOWER_BOUND_NOT_INTEGER = 'game-validation-errors.error-lower-bound-not-integer';
+const ERROR_UPPER_BOUND_NOT_INTEGER = 'game-validation-errors.error-upper-bound-not-integer';
+const ERROR_MARGIN_NOT_INTEGER = 'game-validation-errors.error-margin-not-integer';
 
 export {
     ERROR_ANSWER_NOT_INTEGER,
