eb756efd0f316d50cee06e35ac2c2c33e22abc03
Ikramarroud <ikram.arroud@polymtl.ca>
2025-04-01 22:25:11 -0400

    voting client lourd done

--- server/app/services/match-room/match-room.service.ts
+++ server/app/services/match-room/match-room.service.ts
@@ -79,6 +79,7 @@ export class MatchRoomService {
         this.votesCount ={};
         this.totalVotes = [];
 
+
         const newRoom: MatchRoom = {
             code: roomCode,
             hostSocket: socket,
