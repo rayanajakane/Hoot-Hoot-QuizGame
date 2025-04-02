db6d6e1cac0b107bd03ba74843da30f14e6adbf7
nadabfh <112907983+nadabfh@users.noreply.github.com>
2025-04-02 17:55:49 -0400

    all translation texts done

--- server/app/constants/money-errors.ts
+++ server/app/constants/money-errors.ts
@@ -1,5 +1,5 @@
-const LOW_BALANCE = '.not-enough-money';
-const DONATION_LIMIT_EXCEEDED = '.donation-amount-exceeded';
-const INVALID_AMOUNT = '.invalid-amount';
-const ZERO_AMOUNT = '.zero-amount';
+const LOW_BALANCE = 'money-errors.not-enough-money';
+const DONATION_LIMIT_EXCEEDED = 'money-errors.donation-amount-exceeded';
+const INVALID_AMOUNT = 'money-errors.invalid-amount';
+const ZERO_AMOUNT = 'money-errors.zero-amount';
 export { DONATION_LIMIT_EXCEEDED, INVALID_AMOUNT, LOW_BALANCE, ZERO_AMOUNT };
--- server/app/constants/request-errors.ts
+++ server/app/constants/request-errors.ts
@@ -1,13 +1,13 @@
-const ERROR_GAME_NOT_FOUND = 'Le jeu est introuvable.';
-const ERROR_GAME_SAME_TITLE = 'Un jeu du même titre existe déjà.';
-const ERROR_INVALID_GAME = 'Le jeu est invalide:';
-const ERROR_WRONG_FORMAT = 'Au moins un des éléments du jeu ne suit pas le format attendu.';
+const ERROR_GAME_NOT_FOUND = 'request-errors.error-game-not-found';
+const ERROR_GAME_SAME_TITLE = 'request-errors.error-game-same-title';
+const ERROR_INVALID_GAME = 'request-errors.error-invalid-game';
+const ERROR_WRONG_FORMAT = 'request-errors.error-wrong-format';
 
-const ERROR_QUESTION_NOT_FOUND = 'La question est introuvable.';
-const ERROR_QUESTION_BANK_SAME_TITLE = 'La question existe déjà dans la banque.';
-const ERROR_INVALID_QUESTION = 'La question est invalide:';
+const ERROR_QUESTION_NOT_FOUND = 'request-errors.error-question-not-found';
+const ERROR_QUESTION_BANK_SAME_TITLE = 'request-errors.error-question-same-title-bank';
+const ERROR_INVALID_QUESTION = 'request-errors.error-invalid-question';
 
-const ERROR_DEFAULT = 'Erreur:';
+const ERROR_DEFAULT = 'request-errors.error-default';
 
 export {
     ERROR_DEFAULT,
