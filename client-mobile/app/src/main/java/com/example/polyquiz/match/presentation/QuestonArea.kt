cb7e00417c2ab3f480d3469b4af31ca2af3831ba
nadabfh <112907983+nadabfh@users.noreply.github.com>
2025-04-01 21:22:12 -0400

    questions with images done on tablet

--- client-mobile/app/src/main/java/com/example/polyquiz/match/presentation/QuestonArea.kt
+++ client-mobile/app/src/main/java/com/example/polyquiz/match/presentation/QuestonArea.kt
@@ -10,6 +10,15 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Text
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.draw.clip
+
+import coil.compose.rememberAsyncImagePainter
+
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
@@ -50,6 +59,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
@@ -62,6 +72,7 @@ import com.example.polyquiz.SnackbarEvent
 import com.example.polyquiz.chat.presentation.ChatComponent
 import com.example.polyquiz.constants.AnswerCorrectness
 import kotlinx.coroutines.launch
+import com.example.polyquiz.match.domain.MatchRoomService.isCooldown
 
 @Composable
 fun QuestionArea(
@@ -278,6 +289,7 @@ fun QuestionArea(
                 }
 
             }
+
 
             Spacer(modifier = Modifier.height(12.dp))
             if (context != MatchContext.HOSTVIEW) {
