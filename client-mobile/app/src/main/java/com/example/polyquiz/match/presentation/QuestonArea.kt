ae49dce15ab47e3ce74bbf3ded219b6e9d51a75e
nadabfh <112907983+nadabfh@users.noreply.github.com>
2025-04-01 21:22:12 -0400

    questions with images done on tablet

--- client-mobile/app/src/main/java/com/example/polyquiz/match/presentation/QuestonArea.kt
+++ client-mobile/app/src/main/java/com/example/polyquiz/match/presentation/QuestonArea.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Text
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.draw.clip
 
 import coil.compose.rememberAsyncImagePainter
