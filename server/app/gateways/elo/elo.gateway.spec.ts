f9695ddf1483fa4eb6c03389bc042e0fbdbc2e95
Verocayden <112421679+Verocayden@users.noreply.github.com>
2025-04-03 18:28:49 -0400

    Fixed some server tests

--- server/app/gateways/elo/elo.gateway.spec.ts
+++ server/app/gateways/elo/elo.gateway.spec.ts
@@ -1,18 +1,22 @@
+import { EloService } from '@app/services/elo/elo.service';
 import { Test, TestingModule } from '@nestjs/testing';
+import { createStubInstance, SinonStubbedInstance } from 'sinon';
 import { EloGateway } from './elo.gateway';
 
 describe('EloGateway', () => {
-  let gateway: EloGateway;
+    let gateway: EloGateway;
+    let eloService: SinonStubbedInstance<EloService>;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [EloGateway],
-    }).compile();
+    beforeEach(async () => {
+        eloService = createStubInstance(EloService);
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [EloGateway, { provide: EloService, useValue: eloService }],
+        }).compile();
 
-    gateway = module.get<EloGateway>(EloGateway);
-  });
+        gateway = module.get<EloGateway>(EloGateway);
+    });
 
-  it('should be defined', () => {
-    expect(gateway).toBeDefined();
-  });
+    it('should be defined', () => {
+        expect(gateway).toBeDefined();
+    });
 });
