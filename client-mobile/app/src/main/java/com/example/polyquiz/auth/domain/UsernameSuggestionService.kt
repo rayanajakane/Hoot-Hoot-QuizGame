6e09cf9407dc7a7ed1296702e64e5c3df5e12e91
Ikramarroud <ikram.arroud@polymtl.ca>
2025-04-04 00:43:11 -0400

    fixed issue with usernames not loading

--- client-mobile/app/src/main/java/com/example/polyquiz/auth/domain/UsernameSuggestionService.kt
+++ client-mobile/app/src/main/java/com/example/polyquiz/auth/domain/UsernameSuggestionService.kt
@@ -1,30 +1,39 @@
 package com.example.polyquiz.auth.domain
+
 import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import com.example.polyquiz.core.TranslationService
 import com.example.polyquiz.http.CommunicationService
 import com.example.polyquiz.http.QuestionService.getAll
 import com.google.gson.Gson
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 
 object UsernameSuggestionService {
-    var usernames: List<String> = listOf()
+    val _usernames = MutableStateFlow<List<String>>(emptyList())
+    val usernames: Flow<List<String>> = _usernames.asStateFlow()
     var showUsernameDialog by mutableStateOf(false)
-
     lateinit var translationService: TranslationService
-    val apiService = object : CommunicationService("username-suggestion/:fr") {
+
+    val apiService = object : CommunicationService("username-suggestion/fr") {
         override val apiService: ApiService = retrofit.create(ApiService::class.java)
     }
 
     fun getUsernameSuggestions() {
         apiService.getAll(onSuccess = { response ->
-           // data:List<Any> -> this.usernames = data as List<String>
             val gson = Gson()
-            val usernameList = gson.fromJson(gson.toJson(response), Array<String>::class.java).toList()
-            this.usernames = usernameList},
-            onError = { error -> Log.d("UsernameSuggestionService", "Error: $error")})
+            val usernameList =
+                gson.fromJson(gson.toJson(response), Array<String>::class.java).toList()
+            _usernames.update { usernameList }
+        },
+
+            onError = { error -> Log.d("UsernameSuggestionService", "Error: $error") })
 
     }
 
