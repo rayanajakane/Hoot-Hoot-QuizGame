5831793f40a0626a0a53d9df9041ce690e6edc60
Verocayden <112421679+Verocayden@users.noreply.github.com>
2025-04-06 17:16:13 -0400

    Removed some logs (but kept some others for debugging purposes while we still check for other bugs)

--- server/app/services/player-room/player-room.service.ts
+++ server/app/services/player-room/player-room.service.ts
@@ -124,7 +124,6 @@ export class PlayerRoomService {
             this.matchRoomService.matchRooms[roomIndex].players[playerIndex].isPlaying = false;
             this.matchRoomService.matchRooms[roomIndex].activePlayers--;
         }
-        console.log(this.matchRoomService.matchRooms[roomIndex].players);
     }
 
     deletePlayer(matchRoomCode: string, userId: string): void {
@@ -134,7 +133,6 @@ export class PlayerRoomService {
         this.matchRoomService.matchRooms[roomIndex].players = this.matchRoomService.matchRooms[roomIndex].players.filter((player) => {
             return player.id !== userId;
         });
-        console.log(this.matchRoomService.matchRooms[roomIndex].players);
     }
 
     getBannedPlayers(matchRoomCode: string): string[] {
