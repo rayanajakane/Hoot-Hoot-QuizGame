607784ec97d849d6d86fb6def33f1128c93c0b8b
Verocayden <112421679+Verocayden@users.noreply.github.com>
2025-04-06 17:15:15 -0400

    Tried to fix bug where host leaving and creating room before last player quits would lead to immortal room

--- server/app/services/player-room/player-room.service.ts
+++ server/app/services/player-room/player-room.service.ts
@@ -118,6 +118,7 @@ export class PlayerRoomService {
     }
 
     deletePlayerBySocket(socketId: string): string {
+        console.log('Delete player by socket');
         let foundPlayer: Player;
         let foundMatchRoom: MatchRoom;
         this.matchRoomService.matchRooms.forEach((matchRoom: MatchRoom) => {
@@ -128,6 +129,8 @@ export class PlayerRoomService {
                 foundMatchRoom = foundPlayer ? matchRoom : undefined;
             }
         });
+        console.log(`Found Player: ${foundPlayer}`);
+        console.log(`Found MatchRoom: ${foundPlayer}`);
         if (foundPlayer && foundMatchRoom && !foundMatchRoom.isPlaying) {
             this.deletePlayer(foundMatchRoom.code, foundPlayer.id);
         } else if (foundPlayer && foundMatchRoom && foundMatchRoom.isPlaying) {
@@ -173,6 +176,7 @@ export class PlayerRoomService {
     }
 
     makePlayerInactive(matchRoomCode: string, userId: string): void {
+        console.log('Making player inactive');
         const roomIndex = this.matchRoomService.getRoomIndex(matchRoomCode);
         const playerIndex = this.matchRoomService.getRoom(matchRoomCode).players.findIndex((player: Player) => {
             return player.id === userId;
@@ -182,14 +186,17 @@ export class PlayerRoomService {
             this.matchRoomService.matchRooms[roomIndex].players[playerIndex].isPlaying = false;
             this.matchRoomService.matchRooms[roomIndex].activePlayers--;
         }
+        console.log(this.matchRoomService.matchRooms[roomIndex].players);
     }
 
     deletePlayer(matchRoomCode: string, userId: string): void {
+        console.log('Deleting player');
         const roomIndex = this.matchRoomService.getRoomIndex(matchRoomCode);
         this.matchRoomService.matchRooms[roomIndex].activePlayers--;
         this.matchRoomService.matchRooms[roomIndex].players = this.matchRoomService.matchRooms[roomIndex].players.filter((player) => {
             return player.id !== userId;
         });
+        console.log(this.matchRoomService.matchRooms[roomIndex].players);
     }
 
     getBannedPlayers(matchRoomCode: string): string[] {
