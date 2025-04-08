ed27d9a3d8593ebed77761e3127ac4f0e5e85956
Rayan Ajakane <rayan.ajakane@polymtl.ca>
2025-04-08 17:17:33 -0400

    fixed small mistake on wallpapers

--- client-mobile/app/src/main/java/com/example/polyquiz/constants/Wallpaper.kt
+++ client-mobile/app/src/main/java/com/example/polyquiz/constants/Wallpaper.kt
@@ -2,10 +2,10 @@ package com.example.polyquiz.constants
 
 enum class Wallpaper(val value: String) {
     None("none"),
-    WHITE("https://images.unsplash.com/photo-1741467355504-d2df0dd3ae5d?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
-    CLOUDS("https://images.unsplash.com/photo-1738682081595-7bac257f60cd?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
-    WATER("https://images.unsplash.com/photo-1738869748479-fdcb2b782715?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
-    MOUNTAINS("https://images.unsplash.com/photo-1738597452982-5759da74f68d?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
+    White("https://images.unsplash.com/photo-1741467355504-d2df0dd3ae5d?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
+    Clouds("https://images.unsplash.com/photo-1738682081595-7bac257f60cd?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
+    Water("https://images.unsplash.com/photo-1738869748479-fdcb2b782715?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
+    Mountains("https://images.unsplash.com/photo-1738597452982-5759da74f68d?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
 }
 
 const val WALLPAPER_PRICE = 30
