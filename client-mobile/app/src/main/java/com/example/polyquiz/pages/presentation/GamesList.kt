856ce5ff2218f3d921fa2197512bbac8e584f1d3
Rayan Ajakane <rayan.ajakane@polymtl.ca>
2025-04-12 16:20:31 -0400

    fixed text to be black and added missing translations

--- client-mobile/app/src/main/java/com/example/polyquiz/pages/presentation/GamesList.kt
+++ client-mobile/app/src/main/java/com/example/polyquiz/pages/presentation/GamesList.kt
@@ -38,6 +38,7 @@ import com.example.polyquiz.match.domain.PartyConfig
 import com.example.polyquiz.match.presentation.PartyConfigDialog
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
@@ -265,7 +266,7 @@ fun GameList(modifier: Modifier, navigateToWaitPage: () -> Unit, authViewModel: 
                         modifier = Modifier.weight(1f)
                     ) {
                         Text(
-                            text = "Search Games",
+                            text = stringResource(R.string.search_games),
                             modifier = Modifier.padding(bottom = 8.dp)
                         )
 
@@ -273,7 +274,7 @@ fun GameList(modifier: Modifier, navigateToWaitPage: () -> Unit, authViewModel: 
                             OutlinedTextField(
                                 value = titleQuery,
                                 onValueChange = { titleQuery = it },
-                                label = { Text("Title") },
+                                label = { stringResource(R.string.game_title) },
                                 modifier = Modifier
                                     .weight(0.5f)
                                     .padding(bottom = 8.dp),
@@ -282,7 +283,7 @@ fun GameList(modifier: Modifier, navigateToWaitPage: () -> Unit, authViewModel: 
                             OutlinedTextField(
                                 value = authorQuery,
                                 onValueChange = { authorQuery = it },
-                                label = { Text("Author") },
+                                label = { stringResource(R.string.game_author) },
                                 modifier = Modifier.weight(0.5f),
                                 singleLine = true
                             )
@@ -328,10 +329,12 @@ fun GameList(modifier: Modifier, navigateToWaitPage: () -> Unit, authViewModel: 
                             Text(
                                 text = game.title,
                                 maxLines = 1,
+                                color = MaterialTheme.colorScheme.onBackground
                             )
                             Text(
                                 text = game.authorName ?: "",
                                 maxLines = 1,
+                                color = MaterialTheme.colorScheme.onBackground
                             )
                         }
                     }
