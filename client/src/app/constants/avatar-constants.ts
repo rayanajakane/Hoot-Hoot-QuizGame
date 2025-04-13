ac4373766429cdc607accc79027ebc7cf722cee2
Verocayden <112421679+Verocayden@users.noreply.github.com>
2025-04-12 21:57:37 -0400

    Made avatars no longer download; simply get from existing url (for performance sake)

--- client/src/app/constants/avatar-constants.ts
+++ client/src/app/constants/avatar-constants.ts
@@ -1,16 +1,16 @@
 export enum PresetAvatar {
     Default = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT18iwsdCCbBfpa50-5BmNa_m_BX087_x1oWQ&s',
-    A = 'https://archive.org/download/avatars_202504/a.webp',
-    B = 'https://archive.org/download/avatars_202504/b.webp',
-    C = 'https://archive.org/download/avatars_202504/c.webp',
-    D = 'https://archive.org/download/avatars_202504/d.webp',
+    A = 'https://i.pinimg.com/736x/f8/98/ed/f898ede0463ec8c8885f8e649b2e2462.jpg',
+    B = 'https://i.pinimg.com/736x/fa/9c/53/fa9c53526abe050159657c561f8a6177.jpg',
+    C = 'https://i.pinimg.com/736x/d3/34/a0/d334a0934e503575ed2949303589bd77.jpg',
+    D = 'https://i.pinimg.com/736x/12/cb/bb/12cbbb54e440df721116224c860c6997.jpg',
 }
 
 export enum PremiumAvatar {
-    E = 'https://archive.org/download/avatars_202504/e.webp',
-    F = 'https://archive.org/download/avatars_202504/f.webp',
-    G = 'https://archive.org/download/avatars_202504/g.webp',
-    H = 'https://archive.org/download/avatars_202504/h.webp',
+    E = 'https://i.pinimg.com/736x/d1/e7/c8/d1e7c8cbbc78bf0d3287f4a683b6135c.jpg',
+    F = 'https://i.pinimg.com/736x/da/8a/7e/da8a7ec1b9b04ede1789a9fcead9b70a.jpg',
+    G = 'https://i.pinimg.com/736x/3c/79/fb/3c79fb8e644f5c4de6b61ce3aa59c977.jpg',
+    H = 'https://i.pinimg.com/736x/76/0c/46/760c46860ded4afc9fd12d75ff864a08.jpg',
 }
 
 export enum AvatarState {
