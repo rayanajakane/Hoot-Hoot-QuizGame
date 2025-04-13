4054253ec0cdbe5daba23b9052efc1627e9393bb
Verocayden <112421679+Verocayden@users.noreply.github.com>
2025-04-12 21:57:37 -0400

    Made avatars no longer download; simply get from existing url (for performance sake)

--- client-mobile/app/src/main/java/com/example/polyquiz/constants/ChatConstants.kt
+++ client-mobile/app/src/main/java/com/example/polyquiz/constants/ChatConstants.kt
@@ -2,8 +2,8 @@ package com.example.polyquiz.constants
 
 enum class PresetAvatar (val value: String) {
     DEFAULT("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT18iwsdCCbBfpa50-5BmNa_m_BX087_x1oWQ&s"),
-    A("https://archive.org/download/avatars_202504/a.webp"),
-    B("https://archive.org/download/avatars_202504/b.webp"),
-    C("https://archive.org/download/avatars_202504/c.webp"),
-    D("https://archive.org/download/avatars_202504/d.webp")
+    A("https://i.pinimg.com/736x/f8/98/ed/f898ede0463ec8c8885f8e649b2e2462.jpg"),
+    B("https://i.pinimg.com/736x/fa/9c/53/fa9c53526abe050159657c561f8a6177.jpg"),
+    C("https://i.pinimg.com/736x/d3/34/a0/d334a0934e503575ed2949303589bd77.jpg"),
+    D("https://i.pinimg.com/736x/12/cb/bb/12cbbb54e440df721116224c860c6997.jpg")
 }
